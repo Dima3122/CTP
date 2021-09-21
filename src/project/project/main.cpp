294342d3d354631b89a5eb53b6895cac45e04680
Vasiliy <rubanovvasiliy@github.com>
2021-09-21 20:56:48 +0700

    Add cli11 capability

--- src/project/project/main.cpp
+++ src/project/project/main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 #include "../lib/lib/calc.hpp"
+#include "../../../external/cli11/include/CLI/CLI.hpp"
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
+    CLI::App app{"App description"};
+
+    std::string filename = "default";
+    app.add_option("-f,--file", filename, "A help string");
+
+    CLI11_PARSE(app, argc, argv);
+
     cout << sumTwoFloat(11.4, 5.7) << "\n";
     return 0;
 }
